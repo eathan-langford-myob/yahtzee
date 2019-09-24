cf5f38e49fa9f1e7f95f1aa66a37d66d902d481a
Eathan Langford <eathan.langford@myob.com>
2019-09-24 15:43:06 +1000

    implement logic to passed test

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,7 +1,16 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Game {
+
+    public static ScoreCard scoreCard = new ScoreCard();
+
+    public int calculateScore (String roll, String category){
+    int[] finalRoll = Arrays.stream(roll.split(",")).mapToInt(Integer::parseInt).toArray();
+    return IntStream.of(finalRoll).sum();
+    }
 
     public static void main(String[] args) {
 	// write your code here
--- test/com/company/GameTest.java
+++ test/com/company/GameTest.java
@@ -19,9 +19,16 @@ public class GameTest {
     }
 
     @Test
-    public void shouldReturnSumFromChance() {
+    public void shouldReturn14FromChance() {
         int actual = game.calculateScore("1,1,3,3,6", "chance");
         int expected = 14;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldReturn21FromChance() {
+        int actual = game.calculateScore("4,5,5,6,1", "chance");
+        int expected = 21;
 
         assertEquals(expected, actual);
     }
